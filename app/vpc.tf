9b340940b2b5351218a1ab123084e67282a18203
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2020-11-06 13:27:37 -0700

    update app vpc file

--- app/vpc.tf
+++ app/vpc.tf
@@ -10,9 +10,10 @@ module "vpc" {
   source               = "../modules/vpc/"
   name                 = "nerves-hub-${terraform.workspace}"
   cidr                 = "10.${local.subnet[terraform.workspace]}.0.0/16"
-  public_subnets       = ["10.${local.subnet[terraform.workspace]}.1.0/24", "10.${local.subnet[terraform.workspace]}.2.0/24", "10.${local.subnet[terraform.workspace]}.3.0/24"]
-  private_subnets      = ["10.${local.subnet[terraform.workspace]}.11.0/24", "10.${local.subnet[terraform.workspace]}.12.0/24", "10.${local.subnet[terraform.workspace]}.13.0/24"]
-  database_subnets     = ["10.${local.subnet[terraform.workspace]}.21.0/24", "10.${local.subnet[terraform.workspace]}.22.0/24", "10.${local.subnet[terraform.workspace]}.23.0/24"]
+  azs                  = ["us-east-1a", "us-east-1b"]
+  public_subnets       = ["10.${local.subnet[terraform.workspace]}.1.0/24", "10.${local.subnet[terraform.workspace]}.2.0/24"]
+  private_subnets      = ["10.${local.subnet[terraform.workspace]}.11.0/24", "10.${local.subnet[terraform.workspace]}.12.0/24"]
+  database_subnets     = ["10.${local.subnet[terraform.workspace]}.21.0/24", "10.${local.subnet[terraform.workspace]}.22.0/24"]
   enable_dhcp_options  = true
   enable_dns_hostnames = true
 
