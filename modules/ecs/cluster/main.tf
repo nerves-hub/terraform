77dea32ff52d3998df30a1ff27cd0f94ca87568f
Ryan <ryan.stewart@smartrent.com>
2020-11-10 10:35:27 -0700

    Cleanup and removal after testing

--- modules/ecs/cluster/main.tf
+++ modules/ecs/cluster/main.tf
@@ -8,25 +8,6 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   tags = var.tags
 }
 
-#resource "aws_iam_role_policy" "ecs_instance_role_policy" {
-#  name   = "ecs-instance-role-policy-${var.environment}"
-#  policy = file("${path.module}/templates/ecs-instance-role-policy.json")
-#  role   = aws_iam_role.ecs_role.id
-#}
-#
-#resource "aws_iam_role" "ecs_role" {
-#  name               = "ecs-instance-role-${var.environment}"
-#  assume_role_policy = file("${path.module}/templates/ecs-role.json")
-#}
-#
-#resource "aws_iam_instance_profile" "ecs_instance_profile" {
-#  name = "ecs-instance-profile-${var.environment}"
-#  path = "/"
-#  role = aws_iam_role.ecs_role.name
-#}
-
-
-
 resource "aws_iam_role" "ecs_runtime" {
   name               = "${var.app_name}-${var.role_name}"
   assume_role_policy = data.aws_iam_policy_document.assume_ecs_role.json
@@ -129,37 +110,6 @@ data "aws_iam_policy_document" "ecs_runtime" {
     ]
   }
 }
-
-#resource "aws_security_group_rule" "lb_security_group_all_egress" {
-#  type              = "egress"
-#  from_port         = 0
-#  to_port           = 0
-#  protocol          = "-1"
-#  cidr_blocks       = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.lb_security_group.id
-#}
-#
-#resource "aws_security_group_rule" "lb_security_group_cluster_http_ingress" {
-#  count             = length(var.whitelist)
-#  type              = "ingress"
-#  from_port         = 80
-#  to_port           = 80
-#  protocol          = "tcp"
-#  cidr_blocks       = [element(var.whitelist, count.index)]
-#  security_group_id = aws_security_group.lb_security_group.id
-#  description       = "${element(var.whitelist, count.index)} Access"
-#}
-#
-#resource "aws_security_group_rule" "lb_security_group_cluster_https_ingress" {
-#  count             = length(var.whitelist)
-#  type              = "ingress"
-#  from_port         = 443
-#  to_port           = 443
-#  protocol          = "tcp"
-#  cidr_blocks       = [element(var.whitelist, count.index)]
-#  security_group_id = aws_security_group.lb_security_group.id
-#  description       = "${element(var.whitelist, count.index)} Access"
-#}
 
 resource "aws_cloudwatch_log_group" "app" {
   name              = aws_ecs_cluster.ecs_cluster.name
