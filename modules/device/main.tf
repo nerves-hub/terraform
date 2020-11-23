833bdbf3a6a0b34ff628ec3013bf631a74eb7463
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2020-11-23 14:01:57 -0700

    moar tags

--- modules/device/main.tf
+++ modules/device/main.tf
@@ -70,6 +70,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_db_url" {
   type      = "SecureString"
   value     = "postgres://${var.db.username}:${var.db.password}@${var.db.endpoint}/${var.db.name}"
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_erl_cookie" {
@@ -77,6 +78,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_erl_cookie" {
   type      = "SecureString"
   value     = var.erl_cookie
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_ssl_bucket" {
@@ -84,6 +86,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_ssl_bucket" {
   type      = "String"
   value     = var.ca_bucket
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_log_bucket_name" {
@@ -91,6 +94,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_log_bucket_name" {
   type      = "String"
   value     = var.log_bucket
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_app_name" {
@@ -98,6 +102,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_app_name" {
   type      = "String"
   value     = local.device_app_name
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_cluster" {
@@ -105,6 +110,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_cluster" {
   type      = "String"
   value     = var.cluster.name
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_aws_region" {
@@ -112,6 +118,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_aws_region" {
   type      = "String"
   value     = var.region
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_port" {
@@ -119,6 +126,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_port" {
   type      = "String"
   value     = 80
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_host" {
@@ -126,6 +134,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_host" {
   type      = "String"
   value     = "device.${terraform.workspace}.${var.domain}"
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_bucket_name" {
@@ -133,6 +142,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_s3_bucket_name" {
   type      = "String"
   value     = var.app_bucket
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_secret_key_base" {
@@ -140,6 +150,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_secret_key_base" {
   type      = "SecureString"
   value     = var.secret_key_base
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_ses_port" {
@@ -147,6 +158,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_ses_port" {
   type      = "String"
   value     = "587"
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_ses_server" {
@@ -154,6 +166,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_ses_server" {
   type      = "String"
   value     = "email-smtp.${var.region}.amazonaws.com"
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_smtp_username" {
@@ -161,6 +174,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_smtp_username" {
   type      = "SecureString"
   value     = var.smtp_password
   overwrite = true
+  tags      = var.tags
 }
 
 resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_smtp_password" {
@@ -168,6 +182,7 @@ resource "aws_ssm_parameter" "nerves_hub_device_ssm_secret_smtp_password" {
   type      = "SecureString"
   value     = var.smtp_username
   overwrite = true
+  tags      = var.tags
 }
 
 # Roles
