fce9e91910ead7759e918e1a3f44f4e0d061e8c6
Melissa Greenbaum <melissa.greenbaum@smartrent.com>
2021-06-09 14:04:45 -0700

    ca bucket arn

--- modules/ca/output.tf
+++ modules/ca/output.tf
@@ -2,6 +2,10 @@ output "bucket" {
   value = aws_s3_bucket.ca_application_data.bucket
 }
 
+output "bucket_arn" {
+  value = aws_s3_bucket.ca_application_data.arn
+}
+
 output "host" {
   value = "${aws_service_discovery_service.ca_service_discovery.name}.${var.local_dns_namespace.name}"
 }
